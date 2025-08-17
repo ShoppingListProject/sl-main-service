e4c92122fecd94c9c059a7f92f8c8f5424248e9b
this0is0kuba <jakub.rakus06@gmail.com>
2025-08-17 21:07:53 +0200

    initialization

---
+++ src/main/java/com/jrakus/sl_main_service/SlMainServiceApplication.java
@@ -0,0 +1,12 @@
+package com.jrakus.sl_main_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+    public class SlMainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlMainServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jrakus/sl_main_service/SlMainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrakus.sl_main_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlMainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
