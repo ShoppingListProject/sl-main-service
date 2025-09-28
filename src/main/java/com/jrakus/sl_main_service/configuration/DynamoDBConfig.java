4ffc1eb8ec3d47814c5fe93a37a1ce301b98bfc6
this0is0kuba <jakub.rakus06@gmail.com>
2025-09-28 20:37:58 +0200

    create shopping list repository draft

--- src/main/java/com/jrakus/sl_main_service/configuration/DynamoDBConfig.java
+++ src/main/java/com/jrakus/sl_main_service/configuration/DynamoDBConfig.java
@@ -2,6 +2,7 @@ package com.jrakus.sl_main_service.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -9,19 +10,25 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 import java.net.URI;
 
+@Configuration
 public class DynamoDBConfig {
 
-    @Value("${dynamodb.endpoint}")
-    private String dynamoDbEndpoint;
+    private final String dynamoDbEndpoint;
+    private final String region;
+    private final String accessKeyId;
+    private final String secretKey;
 
-    @Value("${dynamodb.region}")
-    private String region;
-
-    @Value("${aws.accessKeyId}")
-    private String accessKeyId;
-
-    @Value("${aws.secretKey}")
-    private String secretKey;
+    public DynamoDBConfig(
+            @Value("${dynamodb.endpoint}") String dynamoDbEndpoint,
+            @Value("${dynamodb.region}") String region,
+            @Value("${aws.accessKeyId}") String accessKeyId,
+            @Value("${aws.secretKey}") String secretKey
+    ) {
+        this.dynamoDbEndpoint = dynamoDbEndpoint;
+        this.region = region;
+        this.accessKeyId = accessKeyId;
+        this.secretKey = secretKey;
+    }
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
--- src/main/java/com/jrakus/sl_main_service/repositories/ShoppingListRepository.java
+++ src/main/java/com/jrakus/sl_main_service/repositories/ShoppingListRepository.java
@@ -1,7 +1,75 @@
 package com.jrakus.sl_main_service.repositories;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jrakus.sl_main_service.configuration.DynamoDBTableConfig;
+import org.openapitools.model.ShoppingList;
+import org.openapitools.model.ShoppingListItemsPerCategoryInner;
 import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class ShoppingListRepository {
+
+    private final DynamoDbClient dynamoDbClient;
+    private final String tableName;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    private final String pkPrefix = "USER#";
+    private final String skPrefix = "SHOPPING_LIST#";
+
+    public ShoppingListRepository(
+            DynamoDbClient dynamoDbClient,
+            DynamoDBTableConfig dynamoDBTableConfig
+    ) {
+        this.dynamoDbClient = dynamoDbClient;
+        this.tableName = dynamoDBTableConfig.getTableName();
+    }
+
+    public List<ShoppingList> getAllShoppingListsForUser(String userId) {
+
+        String pkValue = this.pkPrefix + userId;
+
+        Map<String, AttributeValue> expressionValues = new HashMap<>();
+        expressionValues.put(":pkVal", AttributeValue.builder().s(pkValue).build());
+        expressionValues.put(":skPrefix", AttributeValue.builder().s(this.skPrefix).build());
+
+        String keyConditionExpression = "PK = :pkVal AND begins_with(SK, :skPrefix)";
+
+        QueryRequest queryRequest = QueryRequest.builder()
+                .tableName(tableName)
+                .keyConditionExpression(keyConditionExpression)
+                .expressionAttributeValues(expressionValues)
+                .build();
+
+        QueryResponse queryResponse = dynamoDbClient.query(queryRequest);
+
+        List<Map<String, AttributeValue>> items = queryResponse.items();
+        List<ShoppingList> shoppingLists = new ArrayList<>();
+
+        queryResponse.items().stream()
+                .map(item -> {
+
+                    ShoppingList shoppingList = new ShoppingList();
+
+                    List<ShoppingListItemsPerCategoryInner> itemsPerCategory = objectMapper.readValue(
+
+                    shoppingList.setName(item.get("name").s());
+                    shoppingList.setCreatedAt(item.get("createdAt").s());
+                    shoppingList.setUpdatedAt(item.get("updatedAt").s());
+                    shoppingList.setItemsPerCategory(item.get("itemsPerCategory"));
+
+                    return shoppingList;
+
+                })
+                .toList();
+    }
+
 }
