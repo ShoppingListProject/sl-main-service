68294d3ea8c9c3dc773b7eab290dd5a4a6bd8e92
this0is0kuba <jakub.rakus06@gmail.com>
2025-10-23 21:37:05 +0200

    remove unsued import

--- src/main/java/com/jrakus/sl_main_service/repositories/dynamo_db/ShoppingListRepositoryDynamoDB.java
+++ src/main/java/com/jrakus/sl_main_service/repositories/dynamo_db/ShoppingListRepositoryDynamoDB.java
@@ -4,7 +4,6 @@ import com.jrakus.sl_main_service.repositories.ShoppingListRepository;
 import com.jrakus.sl_main_service.repositories.dynamo_db.mapper.ShoppingListMapper;
 import com.jrakus.sl_main_service.repositories.dynamo_db.utils.DynamoDBQueryHelper;
 import org.openapitools.model.ShoppingList;
-import org.openapitools.model.ShoppingListBase;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
